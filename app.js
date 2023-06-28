40ead1cdccd0095a5d3e3e437c56acb1d6e512a6
Sola Jegede <sjegede1@gmail.com>
2023-06-27 23:56:05 -0500

    I can fire my lasersgit add -A

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // DOM Elements
-const goodGuy = document.querySelector(".player-sprite");
-const goodGuyPew = document.querySelector('.player-projectile')
+const playerSprite = document.querySelector(".player-sprite");
+const gameScreen = document.querySelector(".game-screen");
+const gameScreenRect = gameScreen.getBoundingClientRect();
+let frameRateDelay = 17 * 0;
+const projectileDiv = document.createElement('div')
+projectileDiv.className = "projectile"
+
 
 // Objects and Global Variables
 keyCodeMap = {
@@ -11,114 +16,141 @@ keyCodeMap = {
 };
 
 // Event Listeners
-document.addEventListener('keydown', (event) => {
-    console.log(event)
-    //I'm using the one statement istead of multiple
-    switch (event.key) {
-        case "ArrowRight":
-            console.log(event.key, ' KEYDOWN');
-            keyCodeMap[event.key].pressed = 1;
-            break;
-        case "ArrowLeft":
-            console.log(event.key, ' KEYDOWN');
-            keyCodeMap[event.key].pressed = 1;
-            break;
-        case "Shift":
-            console.log(event.key, ' KEYDOWN');
-            keyCodeMap[event.key].pressed = 1;
-            break;
-        case " ":
-            console.log(event.code, ' KEYDOWN');
-            keyCodeMap[event.code].pressed = 1;
-            break;
-        default:
-            console.log(event.key, ' KEYDOWN Not configured')
-    }
-})
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowRight":
+      console.log(event.key, " KEYDOWN");
+      keyCodeMap[event.key].pressed = 1;
+      break;
+    case "ArrowLeft":
+      console.log(event.key, " KEYDOWN");
+      keyCodeMap[event.key].pressed = 1;
+      break;
+    case "Shift":
+      console.log(event.key, " KEYDOWN");
+      keyCodeMap[event.key].pressed = 1;
+      break;
+    case " ":
+      console.log(event.code, " KEYDOWN");
+      keyCodeMap[event.code].pressed = 1;
+      break;
+    default:
+      console.log(event.key, " KEYDOWN Not configured");
+  }
+});
 
-document.addEventListener('keyup', (event) => {
-    // console.log(event.key, ' KEYDOWN');
-    // keyCodeMap[event.key].pressed = 0;
-    switch (event.key) {
-        case "ArrowRight":
-            console.log(event.key, ' KEYUP');
-            keyCodeMap[event.key].pressed = 0;
-            break;
-        case "ArrowLeft":
-            console.log(event.key, ' KEYUP');
-            keyCodeMap[event.key].pressed = 0;
-            break;
-        case "Shift":
-            console.log(event.key, ' KEYUP');
-            keyCodeMap[event.key].pressed = 0;
-            break;
-        case " ":
-            console.log(event.code, ' KEYUP');
-            keyCodeMap[event.code].pressed = 0;
-            break;
-        default:
-            console.log(event.key, ' KEYDOWN Not configured')
-    }
-})
+document.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowRight":
+      console.log(event.key, " KEYUP");
+      keyCodeMap[event.key].pressed = 0;
+      break;
+    case "ArrowLeft":
+      console.log(event.key, " KEYUP");
+      keyCodeMap[event.key].pressed = 0;
+      break;
+    case "Shift":
+      console.log(event.key, " KEYUP");
+      keyCodeMap[event.key].pressed = 0;
+      break;
+    case " ":
+      console.log(event.code, " KEYUP");
+      keyCodeMap[event.code].pressed = 0;
+      break;
+    default:
+      console.log(event.key, " KEYDOWN Not configured");
+  }
+});
 
 // Player Class
 class Player {
   constructor() {
     this.health = 100;
     this.speed = 5; // Number of pixels we move on each press
+    let { width, height } = playerSprite.getBoundingClientRect();
     this.positionX = 0;
-    this.projectile = new Projectile(this,{});
+    this.positionY =  height;
+    this.width = width;
+    this.height = height;
+    this.projectiles = [];
+    this.radius = 30;
   }
+
+  checkBoundaries = () => {
+    let currentPosition = playerSprite.getBoundingClientRect();
+    this.positionX = currentPosition.x - gameScreenRect.x;
+    // Check Left and Right
+    if (this.positionX < 0) {
+      playerSprite.style.left = "1px";
+      console.log("Left Boundary");
+      return false;
+    } else if (this.positionX > gameScreenRect.width - this.width) {
+      playerSprite.style.left = `${gameScreenRect.width - this.width -1}px`;
+      console.log("Right Boundary");
+      return false;
+    }
+    return true;
+  };
+
   move = () => {
+    if (!this.checkBoundaries()) {
+      console.log("STOP");
+      return {};
+    }
+
     if (keyCodeMap.ArrowRight.pressed && keyCodeMap.ArrowLeft.pressed) {
       console.log("RIGHT+LEFT");
       return {};
     } else if (keyCodeMap.ArrowRight.pressed) {
       this.positionX += this.speed;
-      goodGuy.style.left = this.positionX + "px";
+      playerSprite.style.left = this.positionX + "px";
     } else if (keyCodeMap.ArrowLeft.pressed) {
       this.positionX -= this.speed;
-      goodGuy.style.left = this.positionX + "px";
+      playerSprite.style.left = `${this.positionX}px`;
     }
   };
+
+  shoot = () => {
+    let projectile = new Projectile(this);
+    if (keyCodeMap.Space.pressed) {
+        projectile.fire()
+        keyCodeMap.Space.pressed = 0;
+        console.log('FIRE!!')
+    }
+    
+  }
 
   update = () => {
     //Log all changes in the character
     this.move();
-    this.projectile.fire()
+    this.shoot()
   };
+
 }
 
 // Projectile Class
 class Projectile {
-    constructor(user,projectileType) {
-        //User is either the enemy or the player that is firing the weapon 
-        this.user = user;
-        this.projectileType = projectileType; //this tells you the projectile damage and image and animation, speed. This will be an object
-        this.isFired = false;
-        this.speed = 1; // Use from object later
-        this.positionX = 0;
+    constructor(Player) {
+        this.player = Player;
+        this.speed = 5;
+        this.radius = 20;
+        this.div = projectileDiv.cloneNode()
+        this.positionX = this.player.positionX
+        this.positionY = this.player.positionY
     }
-    
-    getUserPosition = () => {
-        return this.user.positionX
-    }
+
     fire = () => {
-        this.positionX = this.getUserPosition();
-        if (keyCodeMap.Space.pressed) {
-            console.log("I'M FIRING MY LASER")
-            console.log(goodGuyPew)
-            goodGuyPew.style.left = this.positionX + 'px';
-        }
+        this.div.style.left = `${this.positionX}px`
+        this.div.style.bottom = `${this.positionY}px`
+        gameScreen.append(this.div)
+        this.div.classList.toggle("projectile-shoot")
     }
 }
-
 
 let player = new Player();
 
 const runGame = () => {
   player.update();
-  console.log("running");
   requestAnimationFrame(runGame);
 };
 
